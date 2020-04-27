64c0d3c336cb209d95c46ee5f462ce76c14a35c8
MatthewAndric <37313251+MatthewAndric@users.noreply.github.com>
2020-04-27 13:35:03 +1000

    updated playerboard

--- PlayerBoard.cpp
+++ PlayerBoard.cpp
@@ -3,17 +3,15 @@
 #include "PlayerBoard.h"
 
 PlayerBoard::PlayerBoard(){
-
-    std::vector<char> line2Input(2, '.'); 
-    std::vector<char> line3Input(3, '.'); 
-    std::vector<char> line4Input(4, '.'); 
-    std::vector<char> line5Input(5, '.');
     
-    line1 = '.';
-    line2 = line2Input;
-    line3 = line3Input;
-    line4 = line4Input;
-    line5 = line5Input;
+    std::vector<std::vector<char>> factoryLines{
+        {'.'},
+        {'.','.'},
+        {'.','.','.'},
+        {'.','.','.','.'},
+        {'.','.','.','.','.'},
+        {'.','.','.','.','.'}};
+                                
     }
 
 
